1176892d1086b54fe1a96ed295ceb99b143d2871
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2020-12-23 16:34:12 +0900

    Delete search_m.js

--- js/search_m.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $('.searchIcon').click(function(){
-      $('.searchMobile').fadeIn(); 
-   });
-   $('.closeBtn').click(function(){
-      $('.searchMobile').fadeOut(); 
-   });
-});
