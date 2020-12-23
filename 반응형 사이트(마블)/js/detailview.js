6bf9f7afbc409b71710d8c5bf9b124a58c56dc3b
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2020-12-23 16:39:26 +0900

    반응형 사이트 01
    소재 : 마블 시네마틱 유니버스
    작업툴 : Brackets 및 포토샵
    작업기간 : 2019. 07.01 ~ 2019. 08. 20
    작업인원 : 1명

---
+++ 반응형 사이트(마블)/js/detailview.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('#detailBtn1').click(function () {
+
+        $('#detailView1').css('opacity', '1');
+        $('#detailView1').css('display', 'block');
+    });
+     $('#detailBtn2').click(function () {
+
+        $('#detailView2').css('opacity', '1');
+        $('#detailView2').css('display', 'block');
+    });
+     $('#detailBtn3').click(function () {
+
+        $('#detailView3').css('opacity', '1');
+        $('#detailView3').css('display', 'block');
+    });
+     $('#detailBtn4').click(function () {
+
+        $('#detailView4').css('opacity', '1');
+        $('#detailView4').css('display', 'block');
+    });
+     $('#detailBtn5').click(function () {
+
+        $('#detailView5').css('opacity', '1');
+        $('#detailView5').css('display', 'block');
+    });
+
+
+
+
+});
