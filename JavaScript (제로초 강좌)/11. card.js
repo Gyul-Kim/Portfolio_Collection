cff12eb7ddc3633247cc2f6615ce53566be364ce
Gyul-Kim <70299851+Gyul-Kim@users.noreply.github.com>
2021-01-11 20:00:26 +0900

    11. 카드 뒤집기 게임
    전편_수정요망

---
+++ JavaScript (제로초 강좌)/11. card.js
@@ -0,0 +1,95 @@
+var 가로 = 4;
+var 세로 = 3;
+var 색깔들 = ['red', 'red', 'orange', 'orange', 'green', 'green', 'yellow', 'yellow', 'white', 'white', 'pink', 'pink'];
+var 색깔후보 = 색깔들.slice(); // 백업변수, 참조관계가 되기 때문에 끊어야한다.
+// 객체의 경우, 대입을 하게 되면 참조관계가 된다.
+var 색깔 = [];
+var 클릭플래그 = true;
+var 클릭카드 = [];
+var 완성카드 = [];
+var 시작시간;
+
+function 셔플() { // 피셔에이츠 셔플
+    for (var i = 0; 색깔후보.length > 0; i += 1) {
+        색깔 = 색깔.concat(색깔후보.splice(Math.floor(Math.random() * 색깔후보.length), 1));
+    }
+}
+
+console.log(색깔);
+
+function 카드세팅(가로, 세로) {
+    클릭플래그 = false;
+    for (var i = 0; i < 가로 * 세로; i += 1) {
+        var card = document.createElement('div');
+        card.className = 'card';
+    
+        var cardInner = document.createElement('div');
+        cardInner.className = 'card-inner';
+    
+        var cardFront = document.createElement('div');
+        cardFront.className = 'card-front';
+    
+        var cardBack = document.createElement('div');
+        cardBack.className = 'card-back';
+        cardBack.style.backgroundColor = 색깔[i]
+    
+        cardInner.appendChild(cardFront);
+        cardInner.appendChild(cardBack);
+        card.appendChild(cardInner);
+        (function (c) {
+            card.addEventListener('click', function() {
+                if (클릭플래그 && !완성카드.includes(c)) {
+                    c.classList.toggle('flipped');
+                    클릭카드.push(c);
+                    if (클릭카드.length === 2) {
+                        if (클릭카드[0].querySelector('.card-back').style.backgroundColor === 클릭카드[1].querySelector('.card-back').style.backgroundColor) 
+                        { 
+                            완성카드.push(클릭카드[0]);
+                            완성카드.push(클릭카드[1]);
+                            클릭카드 = [];
+                
+                            if (완성카드.length === 12) {
+                                var 끝시간 = new Date();
+                                 
+                                alert('성공입니다~!' + (끝시간 - 시작시간) / 1000 + '초 걸렸네요~?');
+                                document.querySelector('#wrapper').innerHTML = '';
+                                색깔후보 = 색깔들.slice();
+                                색깔 = [];
+                                완성카드 = [];
+                                시작시간;
+                                셔플();
+                                카드세팅(가로, 세로);
+                            }
+                        } else {  // 두 카드의 색이 다르면
+                            클릭플래그 = false;
+                            setTimeout(function() {
+                                클릭카드[0].classList.remove('flipped');
+                                클릭카드[1].classList.remove('flipped');
+                                클릭플래그 = true;
+                                클릭카드 = [];
+                                }, 1000);
+                            }
+                        }
+                    }
+            }); // 스코프나 클로저 문제 발생할 확률 높음
+        })(card);        
+        document.body.appendChild(card);
+    }
+    
+    document.querySelectorAll('.card').forEach(function (card, index) {
+        setTimeout(function() {
+            card.classList.add('flipped');
+        }, 1000 + 1000 * index);
+    });
+}
+
+setTimeout(function() {
+    document.querySelectorAll('.card').forEach(function (card, index) {
+        card.classList.remove('flipped');
+    });
+    클릭플래그 = true;
+    시작시간 = new Date();
+}, 5000);
+
+셔플();
+카드세팅(가로, 세로);
